fcdb5693f63aa9fef55bce7fece47bdbeb10010e
ArvidW <71834553+ArvidWedtstein@users.noreply.github.com>
2022-01-21 12:43:52 +0100

    c

---
+++ wp-content/themes/Wordpress-Template/footer.php
@@ -0,0 +1,12 @@
+    <footer class="">
+      <p class="">Copyright © Arvid Wedtstein</p>
+      <?php
+        dynamic_sidebar('footer-1');
+      ?>
+    </footer>
+  </div>
+<?php
+  wp_footer();
+?>
+</body>
+</html>
---
+++ wp-content/themes/Wordpress-Template/tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+      dblack: '#141204',
+      dgrey: "#5C6F68",
+      dred: "#EF3E36",
+      ddarkblue: "#0B1D51",
+      dblue: "#5DB7DE"
+      },
+      boxShadow: {
+      'devco': '0 2.8px 2.2px hsl(200 50% 3% / calc(.3 + .03)), 0 6.7px 5.3px hsl(200 50% 3% / calc(.3 + .01)), 0 12.5px 10px hsl(200 50% 3% / calc(.3 + .02)),0 22.3px 17.9px hsl(200 50% 3% / calc(.3 + .02)),0 41.8px 33.4px hsl(200 50% 3% / calc(.3 + .03)), 0 100px 80px hsl(200 50% 3% / .3)',
+      }
+    }
+  }
+}

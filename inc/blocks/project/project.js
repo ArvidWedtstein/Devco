d97bd887365f412ed1a9c9357f6730d99b6e470d
ArvidW <71834553+ArvidWedtstein@users.noreply.github.com>
2022-02-04 13:44:58 +0100

    d

--- inc/blocks/project/project.js
+++ inc/blocks/project/project.js
@@ -5,6 +5,10 @@
 		icon: 'text', // The icon of block in editor.
 		category: 'common', // The category of block in editor.
 		attributes: {
+            content: {
+                source: 'html',
+                selector: 'h2',
+            },
             color: {
                 type: 'string',
                 default: 'lime'
@@ -12,19 +16,90 @@
             fill: {
                 type: 'number',
                 default: 1
-            }
+            },
+            textColor: {
+                type: 'string',
+                default: "#333333"
+            },
         },
+        // https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/
 		edit: function( props ) {
             return (
-                <div { ...useBlockProps()}>
-                    <InspectorControls>
-                        <Panel>
-                            <PanelBody title="Details">
-
-                            </PanelBody>
-                        </Panel>
-                    </InspectorControls>
-                </div>,
+                el(wp.element.Fragment, null, 
+                    el(editor.InspectorControls, null,
+                        el(editor.PanelColorSettings, {
+                            title: wp.i18n.__("Color Settings", "devco"),
+                            colorSettings: [
+                                {
+                                    label: wp.i18n.__("Text Color", "devco"),
+                                    value: props.attributes.textColor,
+                                    onChange: function( newColor ) {
+                                        props.setAttributes({ textColor: newColor });
+                                    }
+                                }
+                            ]
+                        })
+                    ),
+                    el( 'div', { className: 'wp-block-devco-project' },
+                        el( 'div', { className: 'chart' },
+                            el('div', { className: `bar bar-${props.attributes.fill} ${props.attributes.color}`}, 
+                                el('div', { className: 'face top'},
+                                    el('div', { className: 'growing-bar'})
+                                ),
+                                el('div', { className: 'face side-0'},
+                                    el('div', { className: 'growing-bar'})
+                                ),
+                                el('div', { className: 'face floor'},
+                                    el('div', { className: 'growing-bar'})
+                                ),
+                                el('div', { className: 'face side-a'}),
+                                el('div', { className: 'face side-b'}),
+                                el('div', { className: 'face side-1'},
+                                    el('div', { className: 'growing-bar'})
+                                ),
+                                el(
+                                    'select',
+                                    {
+                                        value: props.attributes.color,
+                                        onChange: function( content ) {
+                                            props.setAttributes( { color: content } );
+                                        }, 
+                                        style: {
+                                            color: props.attributes.textColor
+                                        },
+                                    },
+                                    el("option", {value: "red" }, "Red"),
+                                    el("option", {value: "cyan" }, "Cyan"),
+                                    el("option", {value: "lime" }, "Lime")
+                                ),
+                                el(
+                                    'input',
+                                    {
+                                        className: "bg-red-400",
+                                        id: "fill",
+                                        value: props.attributes.fill,
+                                        onChange: function( content ) {
+                                            props.setAttributes( { fill: content.target.value } );
+                                        }
+                                    }
+                                ),
+                            ),
+                        )
+                    )
+                )
+                
+            );
+        },
+		save: function( props ) {
+            return (
+                el( editor.RichText.Content, {
+                    tagName: 'h2', 
+                    value: props.attributes.content,
+                    style: {
+                        backgroundColor: props.attributes.backgroundColor,
+                        color: props.attributes.textColor
+                    },        
+                }), 
                 el( 'div', { className: 'wp-block-devco-project' },
                     el( 'div', { className: 'chart' },
                         el('div', { className: `bar bar-${props.attributes.fill} ${props.attributes.color}`}, 
@@ -42,25 +117,16 @@
                             el('div', { className: 'face side-1'},
                                 el('div', { className: 'growing-bar'})
                             ),
-                            // el(
-                            //     editor.RichText,
-                            //     {
-                            //         tagName: 'div',
-                            //         className: "bg-red-400",
-                            //         id: "red",
-                            //         value: props.attributes.color,
-                            //         onChange: function( content ) {
-                            //             props.setAttributes( { color: content } );
-                            //         }
-                            //     }
-                            // ),
                             el(
                                 'select',
                                 {
                                     value: props.attributes.color,
                                     onChange: function( content ) {
                                         props.setAttributes( { color: content } );
-                                    }
+                                    }, 
+                                    style: {
+                                        color: props.attributes.textColor
+                                    },
                                 },
                                 el("option", {value: "red" }, "Red"),
                                 el("option", {value: "cyan" }, "Cyan"),
@@ -74,53 +140,6 @@
                                     value: props.attributes.fill,
                                     onChange: function( content ) {
                                         props.setAttributes( { fill: content.target.value } );
-                                    }
-                                }
-                            ),
-                        ),
-                    )
-                )
-            );
-        },
-		save: function( props ) {
-            return (
-                el( 'div', { className: 'wp-block-devco-project' },
-                    el( 'div', { className: 'chart' },
-                        el('div', { className: `bar bar-${props.attributes.fill} ${props.attributes.color}`}, 
-                            el('div', { className: 'face top'},
-                                el('div', { className: 'growing-bar'})
-                            ),
-                            el('div', { className: 'face side-0'},
-                                el('div', { className: 'growing-bar'})
-                            ),
-                            el('div', { className: 'face floor'},
-                                el('div', { className: 'growing-bar'})
-                            ),
-                            el('div', { className: 'face side-a'}),
-                            el('div', { className: 'face side-b'}),
-                            el('div', { className: 'face side-1'},
-                                el('div', { className: 'growing-bar'})
-                            ),
-                            el(
-                                editor.RichText,
-                                {
-                                    tagName: 'div',
-                                    className: "bg-red-400",
-                                    id: "red",
-                                    value: props.attributes.color,
-                                    onChange: function( content ) {
-                                        props.setAttributes( { color: content } );
-                                    }
-                                }
-                            ),
-                            el(
-                                'input',
-                                {
-                                    className: "bg-red-400",
-                                    id: "lime",
-                                    value: props.attributes.fill,
-                                    onChange: function( content ) {
-                                        props.setAttributes( { fill: content } );
                                     }
                                 }
                             ),

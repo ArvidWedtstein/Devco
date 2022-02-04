245b9d769e3108464401c96c7a39188fe9b8f194
ArvidW <71834553+ArvidWedtstein@users.noreply.github.com>
2022-02-04 10:36:35 +0100

    w

--- inc/blocks/project/project.js
+++ inc/blocks/project/project.js
@@ -15,67 +15,35 @@
                 default: 'Join Today'
             }
         },
-        /*<div class="chart">
-            <div class="bar bar-30 white">
-              <div class="face top">
-                  <div class="growing-bar"></div>
-              </div>
-              <div class="face side-0">
-                  <div class="growing-bar"></div>
-              </div>
-              <div class="face floor">
-                  <div class="growing-bar"></div>
-              </div>
-              <div class="face side-a"></div>
-              <div class="face side-b"></div>
-              <div class="face side-1">
-                  <div class="growing-bar"></div>
-              </div>
-            </div>
-          </div>*/
 		edit: function( props ) {
             return (
-                el( 'div', { className: 'devco-chart' },
-                    el('div', { className: 'bar bar-30 red'}, 
-                        el('div', { className: 'face top'},
-                            el('div', { className: 'growing-bar'})
-                        ),
-                        el('div', { className: 'face side-0'},
-                            el('div', { className: 'growing-bar'})
-                        ),
-                        el('div', { className: 'face floor'},
-                            el('div', { className: 'growing-bar'})
-                        ),
-                        el('div', { className: 'face side-a'}),
-                        el('div', { className: 'face side-b'}),
-                        el('div', { className: 'face side-1'},
-                            el('div', { className: 'growing-bar'})
+                el( 'div', { className: 'wp-block-devco-project' },
+                    el( 'div', { className: 'chart' },
+                        el('div', { className: 'bar bar-50 red'}, 
+                            el('div', { className: 'face top'},
+                                el('div', { className: 'growing-bar'})
+                            ),
+                            el('div', { className: 'face side-0'},
+                                el('div', { className: 'growing-bar'})
+                            ),
+                            el('div', { className: 'face floor'},
+                                el('div', { className: 'growing-bar'})
+                            ),
+                            el('div', { className: 'face side-a'}),
+                            el('div', { className: 'face side-b'}),
+                            el('div', { className: 'face side-1'},
+                                el('div', { className: 'growing-bar'})
+                            ),
                         ),
                     ),
-                    // el(
-                        
-                    //     editor.RichText,
-                    //     {
-                    //         tagName: 'div',
-                    //         className: 'devco-chart',
-                    //         value: props.attributes.content,
-                    //         onChange: function( content ) {
-                    //             props.setAttributes( { content: content } );
-                    //         }
-                    //     },
-                        
-                    // ),
-                    el(
-                        editor.RichText,
-                        {
-                            tagName: 'span',
-                            className: 'devco-project-button',
-                            value: props.attributes.button,
-                            onChange: function( content ) {
-                                props.setAttributes( { button: content } );
-                            }
-                        }
-                    ),
+                    el('div', { className: 'actions'}, 
+                        el('label', { for: 'red', value: 'Red'}),
+                        el('label', { for: 'cyan', value: 'Cyan'}),
+                        el('label', { for: 'lime', value: 'Lime'}),
+                        el('input', { id: 'red', type: "radio", name: 'switch-color'}),
+                        el('input', { id: 'cyan', type: "radio", name: 'switch-color'}),
+                        el('input', { id: 'lime', type: "radio", name: 'switch-color'}),
+                    )
                 )
             );
         },
